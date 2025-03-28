5cb0fd454f416d7e3b2d28afa99ac16f5c6fff6f
DIEGO FERREIRA ARRUDA <diego_f_arruda@estudante.sesisenai.org.br>
2025-03-27 21:39:32 -0300

    Inicio da SA

---
+++ src/app/(public-routes)/login/page.tsx
@@ -0,0 +1,54 @@
+"use client";
+import { useRouter } from "next/navigation";
+import "./styles.css";
+import { useState } from "react";
+
+export default function Home() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const disabledButton = !email || !(password.length > 8);
+
+  const router = useRouter();
+
+  async function handleSubmit() {
+    router.replace("/");
+  }
+
+  return (
+    <div className="container">
+      <div className="form">
+        <h2>Login</h2>
+        <input
+          type="text"
+          placeholder="E-mail"
+          className="input"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          className="input"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+
+        <button
+          className="button"
+          onClick={handleSubmit}
+          disabled={disabledButton}
+        >
+          Entrar
+        </button>
+      </div>
+      <div className="new-user">
+        <div>
+          <h5>Ainda não possui acesso? Então  </h5>
+        </div>
+        <button className="button-register-user">Cadastre-se</button>
+      </div>
+    </div>
+  );
+}
