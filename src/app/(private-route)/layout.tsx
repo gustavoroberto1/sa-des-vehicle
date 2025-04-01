e73517e57867d2556c7987c66967e2dcebbefab8
gustavoroberto1 <gustavo.roberto@edu.sc.senai.br>
2025-03-31 21:54:35 -0300

    feat: icons

--- src/app/(private-route)/layout.tsx
+++ src/app/(private-route)/layout.tsx
@@ -1,11 +1,12 @@
+'use client '
 import NavMenu from "@/components/NavMenu";
 import { TopMenu } from "@/components/TopMenu";
 import "./styles.css"
 
 export default function PrivateLayout({ children }: { children: React.ReactNode}){
     return (
-        <div >
-            <TopMenu />
+        <div className="layout">
+            <NavMenu  />
             {children}
         </div>
     )
--- src/components/NavMenu/index.tsx
+++ src/components/NavMenu/index.tsx
@@ -1,24 +1,56 @@
+'use client'
+import Link from "next/link"
 import "./styles.css"
+import { usePathname } from "next/navigation"
+import { BsAndroid, BsPerson, BsPersonCircle, BsSquareHalf } from "react-icons/bs";
 
 export default function NavMenu() {
+    const pathName = usePathname();
+
+    
+    const itens = [
+        {
+            label: "Home",
+            page: '/dashboard',
+            icon: <BsPersonCircle />
+        },
+        {
+            label: "Manutenção",
+            page: '/teste',
+            icon: <BsSquareHalf />
+        },
+        {
+            label: "Produção",
+            page: '/teste2',
+            icon: <BsAndroid />
+        },
+        {
+            label: "Estoque",
+            page: '/stock',
+            icon: <BsPersonCircle />
+        },
+        {
+            label: "Qualidade",
+            page: '/quality',
+            icon: <BsPersonCircle />
+        },
+    ]
+
+
+
     return (
         <div className="container">
             <div className="content">
-                <div className="item">
-                    <span>Home</span>
-                </div>
-                <div className="item">
-                    <span>Manutenção</span>
-                </div>
-                <div className="item">
-                    <span>Qualidade</span>
-                </div>
-                <div className="item">
-                    <span>Produção</span>
-                </div>
-                <div className="item">
-                    <span>Estoque</span>
-                </div>
+                {itens.map(item => (
+                    <Link 
+                        key={item.label} 
+                        className={`item ${pathName === item.page ? "selected" : ""}`}
+                        href={item.page}
+                    >
+                        {item.icon}
+                        <span>{item.label}</span>
+                    </Link>
+                ))}
             </div>
         </div>
     )
