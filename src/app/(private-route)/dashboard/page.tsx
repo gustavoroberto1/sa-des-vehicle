97ea13c689cdbf1152add3b60d3bfc2c26bcba63
gustavoroberto1 <gustavo.roberto@edu.sc.senai.br>
2025-04-01 21:28:21 -0300

    feat: cards

--- src/app/(private-route)/dashboard/page.tsx
+++ src/app/(private-route)/dashboard/page.tsx
@@ -1,5 +1,51 @@
-export default function Dashboard(){
+import { MdBuild, MdCheckCircle, MdFactory, MdInventory, MdMoney } from "react-icons/md";
+import styles from './styles.module.css';
+
+export default function Dashboard() {
     return (
-        <h1>Dashboard</h1>
+        <div className={styles.container}>
+            <h1>Dashboard</h1>
+            <div className={styles.card}>
+                <div className={styles.cardItem}>
+                    <div className={`${styles.icon} ${styles.production}`}>
+                        <MdFactory />
+                    </div>
+                    <div className={styles.cardInfos}>
+                        <span>Fabricados</span>
+                        <strong>436</strong>
+                    </div>
+                </div>
+
+                <div className={styles.cardItem}>
+                    <div className={`${styles.icon} ${styles.stock}`}>
+                        <MdInventory />
+                    </div>
+                    <div className={styles.cardInfos}>
+                        <span>Novos Produtos</span>
+                        <strong>436</strong>
+                    </div>
+                </div>               
+                
+                <div className={styles.cardItem}>
+                    <div className={`${styles.icon} ${styles.maintenance}`}>
+                        <MdBuild />
+                    </div>
+                    <div className={styles.cardInfos}>
+                        <span>Manutenções</span>
+                        <strong>436</strong>
+                    </div>
+                </div>               
+                
+                <div className={styles.cardItem}>
+                    <div className={`${styles.icon} ${styles.quality}`}>
+                        <MdCheckCircle />
+                    </div>
+                    <div className={styles.cardInfos}>
+                        <span>Qualidade</span>
+                        <strong>100%</strong>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import '../styles/global.css'
-import NavMenu from "@/components/NavMenu";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const geistSans = Poppins({
+  subsets: ['latin'],
+  weight: ["300", "400", "500", "600", "700"]
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Sa",
@@ -25,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${geistSans.className}`}>
         {children}
       </body>
     </html>
