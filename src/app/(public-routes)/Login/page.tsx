fe447825741f3b43b517a5fa481c46f98d59573f
JOSÉ BRUNO OLIVEIRA DA SILVA <jose-bruno_silva@estudante.sesisenai.org.br>
2025-04-03 19:08:54 -0300

    02/04

--- src/app/(public-routes)/Login/page.tsx
+++ src/app/(public-routes)/Login/page.tsx
@@ -21,7 +21,7 @@ export default function Login() {
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> 
         </body >
     )
 }
