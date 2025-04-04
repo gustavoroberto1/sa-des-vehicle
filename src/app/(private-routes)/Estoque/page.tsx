0e4ebdf93ad2aa2279ce0be89adaab885d79c118
JOSE BRENO OLIVEIRA DA SILVA <jose_bo_silva@estudante.sc.senai.br>
2025-04-03 21:51:11 -0300

    03-04-25

--- src/app/(private-routes)/Estoque/page.tsx
+++ src/app/(private-routes)/Estoque/page.tsx
@@ -1,5 +1,45 @@
-export default function Estoque(){
-    return(
-        <h1>Estoque</h1>
+'use client'
+import { useState } from "react";
+import "./style.css"
+import { BiSearch } from "react-icons/bi";
+import { MdAdd } from "react-icons/md";
+
+
+export default function Estoque() {
+    const [cadastraractivado, setcadastraativado] = useState(false);
+    const [consultaractivado, setconsultarativado] = useState(false);
+
+    const cadastrarativo = () => {
+        setcadastraativado(!cadastraractivado)
+    }
+    const consultarativo = () => {
+        setconsultarativado(!consultaractivado)
+    }
+
+    return (
+        <div className="conteudo">
+            <header className="header">
+                <div className="cadastrar">
+                    <button onClick={cadastrarativo} ><MdAdd /> Cadastrar</button>
+                </div>
+                <div className="consultar">
+                    <button onClick={consultarativo}><BiSearch size={20} /> Consultar</button>
+                </div>
+            </header>
+
+            <div className="conteudo-estoque">
+
+                {cadastraractivado ? (
+                    <h1>Cadastro Ativo</h1>  
+                ) : (
+                    <h1>Conteúdo Inativo</h1>  
+                )}
+                {consultaractivado ? (
+                    <h1>Cadastro Ativo</h1>  
+                ) : (
+                    <h1>Conteúdo Inativo</h1>  
+                )}
+            </div>
+        </div>
     )
 }
