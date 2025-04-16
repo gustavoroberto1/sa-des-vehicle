f3c9f975d06940806c3cc4c49cd031ea4031e62d
JOSE BRENO OLIVEIRA DA SILVA <jose_bo_silva@estudante.sc.senai.br>
2025-04-15 21:51:25 -0300

    15-04-25

--- src/app/(private-routes)/Estoque/page.tsx
+++ src/app/(private-routes)/Estoque/page.tsx
@@ -1,57 +1,14 @@
 'use client'
 import { useState } from "react";
 import "./style.css"
-import { BiSearch } from "react-icons/bi";
-import { MdAdd } from "react-icons/md";
-import ConteudoEstoque from "@/components/Conteudo/index";
-import ConteudoCadastrar from "@/components/Conteudo/index";
-import { register } from "module";
+import RenderConteudo from "@/components/Conteudo"
 
 export default function Estoque() {
-
-
-
-// const [tab, setTab] = useState<"register" | "query">("query")
-// <button onclick={() => setTab(register)} >Cadastrar</button>
-
-
-
-
-
-
-
-
-
-
-
-
-    // const [cadastraractivado, setcadastraativado] = useState(false);
-    // const [consultaractivado, setconsultarativado] = useState(false);
-
-    // const cadastrarativo = () => {
-    //     setcadastraativado(!cadastraractivado)
-    //     setconsultarativado(false)
-    // }
-    // const consultarativo = () => {
-    //     setconsultarativado(!consultaractivado)
-    //     setcadastraativado(false)
-    // }
- 
-//     return (
-//         <div className="conteudo">
-//             <header className="header">
-//                 <div className="cadastrar">
-//                     <button onClick={ConteudoCadastrar} ><MdAdd /> Cadastrar</button>
-
-//                 </div>
-//                 <div className="consultar">
-//                     <button ><BiSearch size={20} /> Consultar</button>
-//                 </div>
-//             </header>
-//             <div className="conteudo-estoque">       
-//                 <ConteudoEstoque />  
-//             </div>
-//         </div>
-//     )
+return(
+    <div className="conteudo-texto">
+        <RenderConteudo />
+    </div>
+)
 
 }
+
